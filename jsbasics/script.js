f80bea912223cf06ab56137612f14d05deb26fe5
ATIFIXATION <atif64896@gmail.com>
2026-04-20 20:38:10 +0530

    js(let,var,comparison,const)

---
+++ jsbasics/script.js
@@ -0,0 +1,31 @@
+console.log("hello world");
+
+
+let c=5;
+
+var b=10;
+var b=25;
+
+
+const pi=3.14;
+
+console.log("a")
+
+// arithmetic  operations 
+let a = 10;
+let b = 5;
+
+console.log(a + b); // 15
+console.log(a - b); // 5
+console.log(a * b); // 50
+console.log(a / b); // 2
+console.log(a % b); // 0 (remainder)
+
+
+
+let e=10;
+let y="10";
+
+console.log(x==y);
+
+console.log(x===y);
