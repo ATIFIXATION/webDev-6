5f5bdb23899465d8a031acabb765505daabd8771
ATIFIXATION <atif64896@gmail.com>
2026-04-29 19:28:51 +0530

    loops in js

---
+++ jsbasics/date29(loops)/script1.js
@@ -0,0 +1,37 @@
+// for(var i=1;i<=10;i++)
+// {
+// console.log("2x" + i + "=" +(2*i));
+
+// }
+
+
+// let a =[24,58,33,86,96,12,53,23,32,100];
+
+// for(let i=0;i<10;i++)
+// {
+// console.log(a[i]);
+// }
+
+// let i=0;
+// while(i<10)
+// {
+//     console.log(a[i])
+// }
+
+
+// var i =0;
+
+// do{
+//     console.log("2x" + i + "=" +(2*i));
+//     i++
+// }
+
+// while(i<=10);
+
+if(i=0)
+{
+    console.log(noo)
+}
+else{
+    console.log(yess)
+}
