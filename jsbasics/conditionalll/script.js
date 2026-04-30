609f782462961ed73bccedb52d1acea522a4f25e
ATIFIXATION <atif64896@gmail.com>
2026-04-30 19:46:55 +0530

    conditional(if else,break,continue,or and  ??)

---
+++ jsbasics/conditionalll/script.js
@@ -0,0 +1,107 @@
+// // if(bath)
+// // {
+// //     console.log("u willl remain clean");
+// // }
+
+// // else
+// // {
+// //     console.log("u will remain dirty");
+// // }
+
+// let n = 5;
+
+// if (n > 0) {
+//   console.log("positive integer");
+// } else {
+//   console.log("negative integer");
+// }
+
+// let a = 20;
+// if (a % 2 == 0) {
+//   console.log("even number");
+// } else {
+//   console.log("odd number");
+// }
+
+// // about marks and grades
+
+// let m = 99;
+
+// if (m >= 90) {
+//   console.log("A");
+// } else if (m > 80) {
+//   console.log("B");
+// } else if (m >= 50) {
+//   console.log("C");
+// } else {
+//   console.log("fail");
+// }
+
+// // ??usernam e and password
+// let usr = "abcde";
+// let pass = "12345";
+// if (usr === "abcde" && pass === "12345") {
+//   console.log("login.success");
+// } else {
+//   console.log("");
+// }
+
+//shorthand
+
+//new omme
+
+// let data;
+
+// let name="atif";
+
+// if(name)
+// {
+//     data=name;
+
+// }
+// else{
+//     data="n/a"
+// }
+// console.log(data);
+
+// let data;
+
+// let name="atif";
+// data=name || "n/a"
+// console.log(data);
+
+// example of break and continue
+
+// for(let i =0;i<5;i++)
+// {
+//     if(i==3)
+//     {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// switch case
+let choice=-2;
+
+switch (choice) {
+  case 1: {
+    console.log("tea");
+    break;
+  }
+
+  case 2: {
+    console.log("lassi");
+    break;
+  }
+
+  case 3: {
+    console.log("coffee");
+    break;
+  }
+
+  default: {
+    console.log("paani");
+    break;
+  }
+}
