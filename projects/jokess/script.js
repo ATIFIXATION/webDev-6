2c8e1aebf070c10dc015f9857d6d2285334a73f1
ATIFIXATION <atif64896@gmail.com>
2026-05-23 20:34:00 +0530

    Add Joke App with HTML structure, JavaScript functionality, and Bootstrap styling

---
+++ projects/jokess/script.js
@@ -0,0 +1,14 @@
+async function getNewJoke() {
+
+    const API_URL = "https://v2.jokeapi.dev/joke/Any";
+
+   const response = await fetch(API_URL);
+
+    const data = await response.json();
+
+
+    document.getElementById("setup").innerText = data.joke || data.setup;
+
+    document.getElementById("delivery").innerText = data.delivery || "";
+
+}
