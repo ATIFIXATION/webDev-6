0dca9f408cfdc26ea5303ceb9c852f2eb6a88563
ATIFIXATION <atif64896@gmail.com>
2026-05-31 09:24:18 +0530

    Add initial React setup with Vite, including SVG assets and main entry point
    - Created react.svg and vite.svg assets in the src/assets directory.
    - Added index.css for global styles.
    - Implemented main.jsx as the entry point for the React application, rendering the App component within StrictMode.
    - Configured Vite with react plugin in vite.config.js for React support.

--- projects/currency_converter/script.js
+++ projects/currency_converter/script.js
@@ -10,51 +10,50 @@ const result = document.getElementById("result");
 
 const amount = document.getElementById("amount");
 
-
 const countryList = {
-    USD: "US",
-    INR: "IN",
-    EUR: "EU",
-    GBP: "GB",
-    JPY: "JP"
+  USD: "US",
+  INR: "IN",
+  EUR: "EU",
+  GBP: "GB",
+  JPY: "JP",
 };
 
-/* Updating flags */
-function updateFlag(element, currency){
-    let countryCode = countryList[currency];
 
-    element.src = `https://flagsapi.com/${countryCode}/flat/64.png`;
+
+/* Updating flags  here the flag will automaticaaly update on its own*/
+function updateFlag(element, currency) {
+  let countryCode = countryList[currency];
+
+  element.src = `https://flagsapi.com/${countryCode}/flat/64.png`;
 }
 
-// changingg flags
+// changingg flags suppose we went into the select option and selected the ind currency,then it will result in changing the lag
 fromCurrency.addEventListener("change", () => {
-    updateFlag(fromFlag, fromCurrency.value);
+  updateFlag(fromFlag, fromCurrency.value);
 });
 
 toCurrency.addEventListener("change", () => {
-    updateFlag(toFlag, toCurrency.value);
+  updateFlag(toFlag, toCurrency.value);
 });
 
 // currency cnverter
 convertBtn.addEventListener("click", async () => {
+  let amt = amount.value;
 
-    let amt = amount.value;
+  if (amt === "" || amt <= 0) {
+    amt = 1;
+    amount.value = 1;
+  }
 
-    if(amt === "" || amt <= 0){
-        amt = 1;
-        amount.value = 1;
-    }
+  const URL = `https://api.exchangerate-api.com/v4/latest/${fromCurrency.value}`;
 
-    const URL = `https://api.exchangerate-api.com/v4/latest/${fromCurrency.value}`;
+  let response = await fetch(URL);
 
-    let response = await fetch(URL);
+  let data = await response.json();
 
-    let data = await response.json();
+  let rate = data.rates[toCurrency.value];
 
-    let rate = data.rates[toCurrency.value];
+  let finalAmount = (amt * rate).toFixed(2);
 
-    let finalAmount = (amt * rate).toFixed(2);
-
-    result.innerText =
-    `${amt} ${fromCurrency.value} = ${finalAmount} ${toCurrency.value}`;
+  result.innerText = `${amt} ${fromCurrency.value} = ${finalAmount} ${toCurrency.value}`;
 });
