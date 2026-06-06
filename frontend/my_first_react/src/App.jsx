81dadcd439e0ee58551a7feeb6bd9956e3b43bfc
ATIFIXATION <atif64896@gmail.com>
2026-06-06 20:06:00 +0530

    Refactor Login component with enhanced styling and form fields; remove unused pages from App component; add new loginpages file

--- frontend/my_first_react/src/App.jsx
+++ frontend/my_first_react/src/App.jsx
@@ -1,24 +1,14 @@
 import Header from "./component/Header";
 import Footer from "./component/Footer";
 
-import Home from "./Home";
-import About from "./pages/About";
-import Product from "./pages/Product";
-import Contactus from "./pages/Contactus";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
 
 function App() {
   return (
     <>
       <Header />
 
-      <Home />
-      <About />
-      <Product />
-      <Contactus />
       <Login />
-      <Register />
 
       <Footer />
     </>
@@ -26,7 +16,3 @@ function App() {
 }
 
 export default App;
-
-
-
-//adding more 
--- frontend/my_first_react/src/pages/Login.jsx
+++ frontend/my_first_react/src/pages/Login.jsx
@@ -1,7 +1,78 @@
+import { useState } from "react";
+
 function Login() {
+
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <>
-      <div>This is Login</div>
+      <div className="container mt-5 ">
+
+        <div className="border p-4 w-50 mx-auto shadow rounded bg-light">
+
+          <h2 className="text-center mb-4">
+            Welcome Back
+          </h2>
+
+          <form className="form-control bg-primary" >
+
+            <div className="mb-3">
+
+              <label className="form-label">
+                Full Name
+              </label>
+
+              <input
+                type="text"
+                className="form-control"
+              />
+
+            </div>
+
+            <div className="mb-3">
+
+              <label className="form-label">
+                Username
+              </label>
+
+              <input
+                type="text"
+                className="form-control"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+              />
+
+            </div>
+
+            <div className="mb-3">
+
+              <label className="form-label">
+                Password
+              </label>
+
+              <input
+                type="password"
+                className="form-control"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+
+            </div>
+
+            <button
+            class="bg-info justify-content-center w-100 border border-success rounded "
+              type="submit"
+              className="btn btn-primary w-100"
+            >
+              Login
+            </button>
+
+          </form>
+
+        </div>
+
+      </div>
     </>
   );
 }
