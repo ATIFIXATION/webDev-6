f26fbaa1b8b76a96ecdbf3d44d9e220f4bd322e8
ATIFIXATION <atif64896@gmail.com>
2026-06-08 00:50:21 +0530

    Refactor Register component: update card styling for improved appearance and consistency

--- frontend/craving_bootstrap/src/pages/Register.jsx
+++ frontend/craving_bootstrap/src/pages/Register.jsx
@@ -14,7 +14,7 @@ function Register() {
       <div className="container  ">
         <div className="row justify-content-center">
           <div className="col-md-6">
-            <div className="card register-glass p-4 shadow m-5">
+            <div className="card register-glass p-4 shadow mt-5 border border-black rounded-4">
               <h1 className="text-center text-danger m-4 border border-black rounded-2 gap-3">
                 Create Account
               </h1>
