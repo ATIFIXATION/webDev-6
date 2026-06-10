4e32e7f96711ed43270f1bc8834d6d7d743cbd75
ATIFIXATION <atif64896@gmail.com>
2026-06-10 20:09:22 +0530

    Refactor code structure for improved readability and maintainability

---
+++ projects/Fakestore/fakestore/src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Products from "./Pages/Products";
+import Contact from "./Pages/Contact";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
