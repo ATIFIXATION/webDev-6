585d4a0f58edeb97035cf78ee5397380efbbffe2
ATIFIXATION <atif64896@gmail.com>
2026-07-15 18:47:27 +0530

    Add loop challenges and examples in JavaScript
    - Introduced various loop constructs including while, do-while, and for loops.
    - Included examples for summing numbers, subtracting values, and iterating over an object.
    - Added a new example demonstrating the use of the for...of loop with an array.

---
+++ all_about_j/day1_related _to_l/loop_challenegs.js
@@ -0,0 +1,76 @@
+
+
+// let sum=0
+// let i=0
+
+// while(i<5)
+// {
+//     sum +=i;
+//     i++
+// }
+
+// console.log(sum)
+
+
+// let sub=0
+// let j=1
+
+// while(j<4)
+// {
+//     sub-=j;
+//     j++
+
+// }
+
+// console.log(sub)
+
+// do while
+
+// let i=0;
+// let n=0
+
+// do
+// {
+//     console.log(i)
+//     i++
+// }
+
+// while(i<5)
+
+
+// for(i=0;i<5;i++)
+// {
+//     console.log(i)
+// }
+
+// let n=prompt("enter ur first natural number");
+// n=parseInt(n)
+// sum=0
+// for(let i=0;i<n ;i++)
+// {
+//     sum+=i
+// }
+
+// console.log(sum)
+
+
+// let obj = {
+
+//     harry:90,
+//     shumila:26,
+//     atif:78
+// }
+
+// for(let a in obj )
+// {
+//     console.log("marks:" + a + obj[a])
+// }
+
+let num = [20,30,40,50,60]
+
+
+
+for(let nomi of num)
+{
+    console.log(nomi*5)
+}
