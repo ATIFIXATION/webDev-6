918137c1011195039643ce8269f47d517daffe5b
ATIFIXATION <atif64896@gmail.com>
2026-07-17 17:57:57 +0530

    feat: add initial HTML and JavaScript files for Day 3 exercises

---
+++ all_about_j/day3/index.js
@@ -0,0 +1,129 @@
+ //arrays
+ 
+//  let  marks = [11,22,55,44]
+// console.log(marks)
+
+
+// console.log(marks[0])
+
+//   marks[4]=88
+
+//   console.log(marks)
+
+
+// let mo =[11,22,44]
+
+// for(let i=0;i<=mo.length;i++)
+// {
+//     console.log(mo[i])
+// }
+
+//array methods
+
+
+// let num =[1,2,3,4]
+
+// let b= num.toString()
+// console.log(b)
+
+// let bum = [1,3,4,6]
+
+// let c=num.join("_")
+// console.log(c)
+
+// let d= bum.pop()
+// console.log(d)
+
+// let e =bum.push(77)
+// console.log(bum)
+
+// let k = bum.shift()
+
+// console.log(bum)
+
+// let r =bum.concat(num)
+// console.log(r)
+
+// const ik=num.slice(2)
+// console.log(ik)
+
+
+// usning loops with array
+
+// let num =[11,22,55,56]
+// for(let i=0;i<=num.length;i++)
+// {
+// console.log(num[i])
+// }
+
+//for each loops
+
+// num.forEach((Element) =>  {
+
+//     console.log(Element*Element)
+// })
+
+//arr.from
+
+// let name ="atif"
+
+// let arr =Array.from(name)
+
+// console.log(arr)
+
+// // for of
+
+
+// for(let i of num)
+// {
+//     console.log(i)
+// }
+
+
+// //for i
+// for(let j of num)
+// {
+//     console.log(j)
+// }
+
+
+//map filter reduce
+
+
+let arr = [11,22,44,5]
+
+
+// arr.map((value) => {
+
+// console.log(value*value)
+
+
+// })
+// console.log(arr)
+
+//array filter 
+
+// let arr2 =[11,22,55,44]
+
+// let a=arr2.filter((a)     =>    
+// {
+//     return a<14
+// })
+
+// console.log(a)
+
+
+//array reduce method
+
+let arr3 =[1,2,2,44]
+
+let g =arr3.reduce((h,h1)=>
+
+{
+    return  h +h1
+}
+
+
+)
+console.log(g)
+
