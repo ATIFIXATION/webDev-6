5cc8210bd590a38d9fabbc866ac6b60ce36409fe
ATIFIXATION <atif64896@gmail.com>
2026-07-17 16:56:37 +0530

    Add initial HTML and JavaScript files for Day 2 exercises
    - Created index.html with basic structure and linked script.js
    - Added script.js with commented-out code snippets for various JavaScript concepts

---
+++ all_about_j/day2/script.js
@@ -0,0 +1,83 @@
+// let marks = {
+//   harry: 90,
+//   shubham: 9,
+//   lovesh: 56,
+//   monika: 4
+// };
+
+// for (let a in marks) {
+//   console.log("marks of students are:" + a + marks[a]);
+// }
+
+
+// let cn =4
+
+// let i
+
+// while(i!=cn)
+
+//     {
+//         i=prompt("enter a correct number")
+//     }
+
+//     console.log("u have enetered a corrcect no")
+
+
+
+
+
+// const mean = (a,b,c,d) => {
+//     return(a+b+c+d) / 4
+// }
+
+// console.log(mean(4,5,6,7))
+
+// const addi=(r,f,g) =>
+// {
+//     return (r+f+g)
+// }
+
+// console.log(addi(5,6,7))
+
+
+//string
+
+// let name ="harry"
+// console.log(name.length)
+
+// let boy1 = "atif"
+// let boy2 =" hamza"
+
+// let sentence =`${boy1} is a friend of ${boy2}`
+
+// console.log(sentence)
+
+// //escape sequence characters
+
+// let fruit = 'bana\'na'
+// console.log(fruit)
+
+//string methods
+
+// let name ="aaron"
+
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.slice(1,2))
+
+// console.log(name.replace("aa","bb"))
+
+// let name ="atif"
+
+// let friend= "hamza"
+
+// console.log(name.concat(friend))
+
+let m=4
+let i="airf"
+
+for(let  i=0; i<4;i++)
+{
+    console.log(i.length)
+}
