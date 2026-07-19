84a42b030ab4a9e2bbfcb6b49c1c6666e451bdd9
ATIFIXATION <atif64896@gmail.com>
2026-07-19 16:03:11 +0530

    feat: add HTML and JavaScript files for Day 4 exercises

---
+++ all_about_j/day4/script.js
@@ -0,0 +1,114 @@
+//practice set
+
+//question 1
+// let arr= [11,22,44,55]
+
+// let a = prompt('enter a number')
+// a = Number.parseInt(a)
+// arr.push(a)
+// console.log(arr)
+
+//question 2
+
+// while(a!=0)
+// {
+//     console.log(arr)
+// }
+
+// console.log(arr)
+
+//using filter  function in array 
+
+// let b = [11, 22, 33, 44, 3];
+
+// let c = b.filter((f) => {
+// return(f < 10);
+// });
+
+
+// console.log(c);
+
+// let d= b.map((value) =>
+
+// {
+//     console.log(value*value)
+// }
+
+// )
+// console.log(d)
+
+
+//console log object
+
+// console.error('hey this is an error')
+
+// console.assert(5>56)
+
+// console.clear
+
+// console.time("for loop")
+
+
+// for(let i =0;i<7;i++)
+// {
+//     console.log(2)
+// }
+
+// console.timeEnd("for loop")
+
+// alert("hello ur script works")
+
+
+// let a =prompt("enter  no here")
+// a= parseInt(a)
+// document.write(a)
+
+// let write = confirm('do u wanna write something')
+// console.log(write)
+
+// console.log(window)
+
+// console.log(document.body)
+// document.body.style.background="green"
+
+
+// alert('harry')
+
+
+// let age = prompt ('enter age ')
+
+// age  =parseInt(age)
+
+// const canDrive=(age)=> {
+//   return age>18? true :false 
+
+// }
+
+// if (canDrive(age)) {
+//     alert ('u can drive')
+// }
+
+// else 
+// {
+//     alert("u cannot drive")
+// }
+
+
+// let marks =80;
+
+// let result = marks>80 ? "true":"false"
+// return result
+
+
+// let driveage= prompt("enter ut age")
+
+// ali =parseInt(driveage)
+
+
+
+// let driveage=18
+
+// let drivii = driveage>=18? "u can drive": "u cannot"
+
+// console.log(drivii)
+
