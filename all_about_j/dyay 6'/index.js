7aefb1539cab0125175e8d5fb9aeed2fdc5cfeff
ATIFIXATION <atif64896@gmail.com>
2026-07-23 23:42:57 +0530

    feat: Add initial HTML, CSS, and JavaScript files for Day 6 exercises

---
+++ all_about_j/dyay 6'/index.js
@@ -0,0 +1,12 @@
+document.getElementsByTagName("nav")[0].firstElementChild.style.color = "red";
+
+document.getElementsByTagName("nav")[0].firstChild.style.color = "green";
+
+document.getElementsByTagName("nav")[0].lastElementChild.style.color =
+  "green";
+
+
+  Document.getElementsByTagName("li").forEach((element) => {
+    Element.style.background ="red"
+
+  })
