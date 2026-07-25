97dbd25e20b790d154b8ced4209d4a56f36a4c0c
ATIFIXATION <atif64896@gmail.com>
2026-07-25 19:51:35 +0530

    refactor: Comment out unused navigation code and clean up JavaScript

--- all_about_j/dyay 6'/index.js
+++ all_about_j/dyay 6'/index.js
@@ -1,12 +1,18 @@
-document.getElementsByTagName("nav")[0].firstElementChild.style.color = "red";
+// document.getElementsByTagName("nav")[0].firstElementChild.style.color = "red";
 
-document.getElementsByTagName("nav")[0].firstChild.style.color = "green";
+// document.getElementsByTagName("nav")[0].firstChild.style.color = "green";
 
-document.getElementsByTagName("nav")[0].lastElementChild.style.color =
-  "green";
+// document.getElementsByTagName("nav")[0].lastElementChild.style.color =
+//   "green";
 
 
-  Document.getElementsByTagName("li").forEach((element) => {
-    Element.style.background ="red"
+//   Document.getElementsByTagName("li").forEach((element) => {
+//     Element.style.background ="red"
 
-  })
+//   })
+
+
+// today we will learn bout inner html and outer html #41 
+
+
+console.log(document.getElementsByTagName)
