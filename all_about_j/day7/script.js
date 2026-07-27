56fd2e7c652d79966fbd3bee5977b25c5c56b3d4
ATIFIXATION <atif64896@gmail.com>
2026-07-27 21:37:29 +0530

    feat: Add initial HTML, CSS, and JavaScript files for Day 7 exercises

---
+++ all_about_j/day7/script.js
@@ -0,0 +1,31 @@
+// let abba= first.getAttribute("class");
+// console.log(abba);
+
+// console.log(first.hasAttribute("class"));
+// console.log(first.hasAttribute("style"));
+
+// // first.setAttribute("hidden","true  sach")
+
+// first.removeAttribute("first")
+
+// // first.element.attributes
+
+
+// console.log(first.player)
+
+
+
+
+// let a = document.getElementsByTagName('div')[0]
+//   console.log(a)
+
+//   let div = document.createElement('div')
+
+//   div.innerhtml = '<h1> helllo  world</h1>'
+// a.append(div);
+// a.prepend(div)
+
+
+let  a = first.insertAdjacentHTML('beforebegin', '<div class ="test"> beforebegin</div> ')
+
+console.log(a)
