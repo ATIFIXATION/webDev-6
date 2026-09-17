f5e82ed19b6da0125aff7be3a4f81d01b1b4dafa
ATIFIXATION <atif64896@gmail.com>
2026-09-17 23:20:11 +0530

    feat: Add initial HTML, CSS, and JavaScript files for Day 8 exercises

---
+++ all_about_j/day8/script.js
@@ -0,0 +1,46 @@
+// first.className="text-black red";
+
+// alert("hello");
+
+// setTimeout(function() {
+//     alert("Time's up!");
+// }, 3000);       
+
+
+// setTimeout(function() {
+
+//     alert("hello this is me");
+   
+// },3000 );
+
+
+// let x=btn.addEventlistener('click',function(e)
+// {
+// alert("hello this is me 1");
+// })
+// let a=prompt("what is ur number ")
+
+// if (a=="2")
+// {
+//     btn.removeEventlistener('click',x)
+// }
+
+// btn.addEventlistener('click',function(e)
+// {
+// alert("hello this is me 2");
+// })
+
+// let a= prompt('what is ur name')
+
+// let b=prompt('how r u doing')
+
+// //async function
+
+//     setTimeout(function() {
+//     alert('yo i m good')
+
+// },2000)
+
+
+
+// function loadscript(url,callback )
